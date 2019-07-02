a60f065764a2162b9aa907b1a155ae9519959d42
Alejandra Gómez <oshingc@gmail.com>
2019-07-01 21:57:37 -0500

    Update for UI colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RecommendationsComponent } from './components/recommendations/recommendations.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './nav/footer/footer.component';
+import { HeaderComponent } from './nav/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RecommendationsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
